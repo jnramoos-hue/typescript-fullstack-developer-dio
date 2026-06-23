a83d672be4a1b2013fd7024105310363ad39b15e
Junior <jnramoos@icloud.com>
2026-06-23 10:02:33 +0100

    Add ts-node-dev, update app.ts, remove index.js
    Add ts-node-dev to devDependencies and a "dev" script to run modules/01-typescript-fundamentals/js/app.ts for live TypeScript development. Replace the simple soma example with a larger TypeScript tutorial file (comments, examples, and array demos) in app.ts and remove the generated JS file modules/01-typescript-fundamentals/js/index.js to avoid keeping compiled output in the repo.

--- modules/01-typescript-fundamentals/js/app.ts
+++ modules/01-typescript-fundamentals/js/app.ts
@@ -1,5 +1,111 @@
-const soma = (a: number, b: number) => {
-    console.log(a + b);
-}
+//---------------------------------------------------------------------
+//Variaveis e tipos
 
-soma(1,2)
+// let b = 'b'
+// const c = 'c'
+//
+// let m: any = 2
+// m = 'Junior'
+// m = false
+
+
+//---------------------------------------------------------------------
+// Objetos e interface
+
+// interface  Person {
+//     name: string
+//     age: number
+//     profission?: string | undefined
+// }
+//
+// const person: Person = {
+//     name: 'Joe',
+//     age: 28
+// }
+//
+// const otherPerson: Person = {
+//     name: 'Jose',
+//     age: 30,
+//     profission: 'Engineer',
+// }
+//
+// const arrayPerson: Array<Person> = [
+//     person,
+//     otherPerson
+// ]
+
+// const arrayNum: number[] = [
+//     1, 2, 3
+// ]
+//
+// const arrayString: Array<string> = [
+//     '1', '2', '3'
+// ]
+
+//---------------------------------------------------------------------
+// Decisões e repetições...
+
+// const num: number = 15;
+//
+// if (num > 15) {
+//     console.log('Num maior que 15')
+// }else if (num === 15){
+//     console.log('Num igual 15')
+// } else {
+//     console.log('Num menor que 15')
+// }
+
+// const typeUser = {
+//     admin: 'Seja bem vindo admin',
+//     student: 'Você é um estudante',
+//     viewer: 'Voce pode visualizar'
+// }
+//
+// function validadeUser(user: string){
+//     console.log(typeUser[user as keyof typeof typeUser]);
+// }
+//
+// const usuario = 'admin'
+//
+// validadeUser(usuario);
+// validadeUser('student');
+// validadeUser('viewer');
+
+//---------------------------------------------------------------------
+// Estrutura de repetição
+// For
+//
+// for(let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+//
+// let n = 2
+// while (n > 6 ){
+//     console.log(n)
+//     n++
+// }
+
+
+//---------------------------------------------------------------------
+// Array
+
+const array: Array<any> = [1, 2, 3, 4]
+
+const stringArray: string[]  = ['a', 'b', 'c']
+
+// console.log(array [0])
+
+console.log('array original: ', array)
+
+array.push(5)
+
+console.log('Array com o novo elemento: ', array)
+
+array.pop()
+
+console.log('Array retirando o ultimo elemento: ', array)
+
+//---------------------------------------------------------------------
+
+
+
